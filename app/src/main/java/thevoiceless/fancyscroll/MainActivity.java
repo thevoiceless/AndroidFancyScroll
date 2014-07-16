1bd1c33ca68e2434d9a0e724abbbf76d1ba06cf7
Riley Moses <victi.resurgo@gmail.com>
2014-07-16 14:09:03 -0600

    Change opacity with scroll and nav drawer open/close

--- app/src/main/java/thevoiceless/fancyscroll/MainActivity.java
+++ app/src/main/java/thevoiceless/fancyscroll/MainActivity.java
@@ -1,57 +1,175 @@
 package thevoiceless.fancyscroll;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.WindowCompat;
-import android.util.Log;
-import android.view.Menu;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Display;
 import android.view.MenuItem;
-import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.View;
-import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.ScrollView;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity implements ScrollViewListener {
 
+    private static final String KEY_SCROLL_POS = "KEY_SCROLL_POS";
+
+    private static int actionBarOpacity = 0;
+
+    private DrawerLayout drawer;
+    private ListView drawerList;
+    private ActionBarDrawerToggle drawerToggle;
+    private ObservableScrollView scrollView;
     private ImageView cover;
-    private Drawable bg;
+    private Drawable actionBarBG;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(WindowCompat.FEATURE_ACTION_BAR_OVERLAY);
         setContentView(R.layout.activity_main);
-        bg = getResources().getDrawable(R.drawable.ab);
-        bg.setAlpha(0);
-        getActionBar().setBackgroundDrawable(bg);
 
-        final ObservableScrollView scrollView = ((ObservableScrollView) findViewById(R.id.scroll));
+        actionBarBG = getResources().getDrawable(R.drawable.ab);
+        drawer = ((DrawerLayout) findViewById(R.id.drawer));
+        drawerList = ((ListView) findViewById(R.id.drawer_list));
+        drawerToggle = new ActionBarDrawerToggle(this, drawer, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                actionBarBG.setAlpha(mapFractionToAlpha(slideOffset, actionBarOpacity));
+                super.onDrawerSlide(drawerView, slideOffset);
+            }
+        };
+        drawer.setDrawerListener(drawerToggle);
+        drawer.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        scrollView = ((ObservableScrollView) findViewById(R.id.scroll));
         cover = ((ImageView) findViewById(R.id.cover));
 
-        scrollView.setScrollViewListener(this);
+        scrollView.setOnScrollListener(this);
+        setActionBarOpacity(actionBarOpacity);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setImageViewScaling();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_SCROLL_POS, scrollView.getScrollY());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        final int position = savedInstanceState.getInt(KEY_SCROLL_POS);
+
+        scrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.scrollTo(0, position);
+
+                if (drawer.isDrawerOpen(drawerList)) {
+                    actionBarBG.setAlpha(mapFractionToAlpha(1f, actionBarOpacity));
+                }
+            }
+        });
+
+//        if (scrollView.getViewTreeObserver().isAlive()) {
+//            scrollView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+//                @Override
+//                public void onGlobalLayout() {
+//                    scrollView.scrollTo(0, position);
+//
+//                    if (scrollView.getViewTreeObserver().isAlive()) {
+//                        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+//                            scrollView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+//                        } else {
+//                            scrollView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+//                        }
+//                    }
+//                }
+//            });
+//        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldX, int oldY) {
         float percentHidden = (scrollView.getScrollY() / (float) cover.getHeight());
-        bg.setAlpha(mapHiddenPercentToAlpha(percentHidden));
-        getActionBar().setBackgroundDrawable(bg);
+        setActionBarOpacity(mapFractionToAlpha(percentHidden));
 
         int top = scrollView.getScrollY() / 2;
         cover.setPadding(0, top, 0, 0);
     }
 
-    private int mapHiddenPercentToAlpha(float percentHidden) {
-        if (percentHidden < 0f) {
-            percentHidden = 0f;
+    private void setImageViewScaling() {
+        Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+        if (display.getRotation() == Surface.ROTATION_0 || display.getRotation() == Surface.ROTATION_180) {
+            // Portrait, use "center"
+            cover.setScaleType(ImageView.ScaleType.CENTER);
+        } else {
+            // Landscape, use "centerCrop"
+            cover.setScaleType(ImageView.ScaleType.CENTER_CROP);
         }
-        if (percentHidden > 1f) {
-            percentHidden = 1f;
+    }
+
+    private void setActionBarOpacity(int opacity) {
+        actionBarOpacity = opacity;
+        actionBarBG.setAlpha(actionBarOpacity);
+        getActionBar().setBackgroundDrawable(actionBarBG);
+    }
+
+    private int mapFractionToAlpha(float alphaFraction) {
+        if (alphaFraction < 0f) {
+            alphaFraction = 0f;
+        }
+        if (alphaFraction > 1f) {
+            alphaFraction = 1f;
         }
 
-        return Math.round(percentHidden * 255);
+        return Math.round(alphaFraction * 255);
+    }
+
+    private int mapFractionToAlpha(float alphaFraction, int startAlpha) {
+        if (alphaFraction < 0f) {
+            alphaFraction = 0f;
+        }
+        if (alphaFraction > 1f) {
+            alphaFraction = 1f;
+        }
+
+        int alphaRange = 255 - startAlpha;
+        int scaledToRange = Math.round(alphaFraction * alphaRange);
+        return startAlpha + scaledToRange;
     }
 }
--- app/src/main/java/thevoiceless/fancyscroll/ObservableScrollView.java
+++ app/src/main/java/thevoiceless/fancyscroll/ObservableScrollView.java
@@ -2,6 +2,7 @@ package thevoiceless.fancyscroll;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.ViewTreeObserver;
 import android.widget.ScrollView;
 
 /**
@@ -9,7 +10,7 @@ import android.widget.ScrollView;
  */
 public class ObservableScrollView extends ScrollView {
 
-    private ScrollViewListener scrollViewListener;
+    private ScrollViewListener onScrollListener;
 
     public ObservableScrollView(Context context) {
         super(context);
@@ -23,16 +24,19 @@ public class ObservableScrollView extends ScrollView {
         super(context, attrs, defStyle);
     }
 
-    public void setScrollViewListener(ScrollViewListener listener) {
-        scrollViewListener = listener;
+    public void setOnScrollListener(ScrollViewListener listener) {
+        onScrollListener = listener;
+    }
+
+    public void setOnGlobalLayoutListener(ViewTreeObserver.OnGlobalLayoutListener listener) {
     }
 
     @Override
     protected void onScrollChanged(int x, int y, int oldX, int oldY) {
         super.onScrollChanged(x, y, oldX, oldY);
 
-        if (scrollViewListener != null) {
-            scrollViewListener.onScrollChanged(this, x, y, oldX, oldY);
+        if (onScrollListener != null) {
+            onScrollListener.onScrollChanged(this, x, y, oldX, oldY);
         }
     }
 }
